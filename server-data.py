a07612e2747157c0d7ceb5557a65408accda32d7
soybean217 <13565644@qq.com>
2017-01-09 20:42:02 +0800

    add command and mobile match

--- server-data.py
+++ server-data.py
@@ -38,18 +38,52 @@ def make_app():
 class SmsHandler(tornado.web.RequestHandler):
     def get(self,sms):
         self.write("ok")
-        print sms
         _ip = self.request.headers["X-Real-IP"] if self.request.headers["X-Real-IP"] != None and len(self.request.headers["X-Real-IP"])>0 else self.request.remote_ip
         _sms_info = {'spcode':sms,'spnumber':self.get_argument('spnumber'),'mobile':self.get_argument('mobile'),'linkid':self.get_argument('linkid'),'msg':self.get_argument('msg'),'status':self.get_argument('status'),'ip':_ip}
-        t = threading.Thread(target=insert_req_log(_sms_info))
-        t.start() 
+        _t = threading.Thread(target=insert_sms_log(_sms_info))
+        _t.start() 
+        _t1 = threading.Thread(target=proc_sms(_sms_info))
+        _t1.start() 
 
-def insert_req_log(_sms_info):
+def insert_sms_log(_sms_info):
     _log_id = 101
     dbLog=torndb.Connection(config.GLOBAL_SETTINGS['log_db']['host'],config.GLOBAL_SETTINGS['log_db']['name'],config.GLOBAL_SETTINGS['log_db']['user'],config.GLOBAL_SETTINGS['log_db']['psw'])
-    sql = 'insert into log_async_generals (`id`,`logId`,`para01`,`para02`,`para03`,`para04`,`para05`,`para06`,`para07`) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-    dbLog.insert(sql,int(round(time.time() * 1000)),_log_id,_sms_info["ip"],_sms_info["spcode"],_sms_info["spnumber"],_sms_info["mobile"],_sms_info["linkid"],_sms_info["msg"],_sms_info["status"])
+    _sql = 'insert into log_async_generals (`id`,`logId`,`para01`,`para02`,`para03`,`para04`,`para05`,`para06`,`para07`) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+    dbLog.insert(_sql,int(round(time.time() * 1000)),_log_id,_sms_info["ip"],_sms_info["spcode"],_sms_info["spnumber"],_sms_info["mobile"],_sms_info["linkid"],_sms_info["msg"],_sms_info["status"])
     return
+
+def proc_sms(_sms_info):
+    try:
+        _sms_cmd = get_cmd(_sms_info)
+        _user = get_user_by_mobile(_sms_info['mobile'])
+        if _user == None :
+            print "can not match user by mobile:" + _sms_info['mobile']
+        else:
+            return
+    except "ParameterError",_argument:
+        print "ParameterError:", _argument
+    else:
+        return
+
+def get_cmd(_sms_info):
+    dbConfig=torndb.Connection(config.GLOBAL_SETTINGS['config_db']['host'],config.GLOBAL_SETTINGS['config_db']['name'],config.GLOBAL_SETTINGS['config_db']['user'],config.GLOBAL_SETTINGS['config_db']['psw'])
+    _sql = 'SELECT spNumber as spnumber,msg,price FROM `sms_cmd_configs` WHERE spNumber = %s and msg = %s'
+    _record = dbConfig.get(_sql, _sms_info['spnumber'], _sms_info['msg']) 
+    if _record==None:
+        raise Exception("ParameterError", "can not match cmd,spcode:"+_sms_info['spcode']+",spnumber:"+_sms_info['spnumber']+",msg:"+_sms_info['msg'])
+        return
+    else:
+        return _record
+
+def get_user_by_mobile(_mobile):
+    dbConfig=torndb.Connection(config.GLOBAL_SETTINGS['config_db']['host'],config.GLOBAL_SETTINGS['config_db']['name'],config.GLOBAL_SETTINGS['config_db']['user'],config.GLOBAL_SETTINGS['config_db']['psw'])
+    _sql = 'SELECT * FROM `imsi_users` WHERE mobile = %s'
+    _record = None
+    if len(_mobile)==11:
+        _record = dbConfig.get(_sql, '86'+_mobile) 
+    if _record == None:
+        _record = dbConfig.get(_sql, _mobile) 
+    return _record
 
 if __name__ == "__main__":
     print "begin..."
