2717176e54ab54ad1cf3d8ba41d48ca2c91d17a8
soybean217 <13565644@qq.com>
2017-08-29 13:29:51 +0800

    凯信源支持增加

--- server-data.py
+++ server-data.py
@@ -107,7 +107,7 @@ class RegisterHandler(tornado.web.RequestHandler):
                 'linkid'), 'msg': self.get_argument('msg'), 'status': self.get_argument('delivrd'), 'ip': _ip, 'para': self.get_argument('ccpara')}
         elif spCode == 'kaixingyuan':
             info = {'spcode': spCode, 'spnumber': self.get_argument('spnumber'), 'mobile': self.get_argument('mobile'), 'linkid': self.get_argument(
-                'orderId'), 'msg': self.get_argument('cmd'), 'status': 'delivrd', 'ip': _ip, 'para': self.get_argument('ccpara')}
+                'orderId'), 'msg': self.get_argument('cmd'), 'status': 'delivrd', 'ip': _ip, 'para': self.get_argument('cpparm')}
         else:
             print('error : no interface')
             return
